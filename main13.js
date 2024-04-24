5f26687bca707b6b2bd0786767ba77d64e55f5af
MAHAM42 <161121868+MAHAM42@users.noreply.github.com>
2024-04-24 11:30:20 -0700

    13-Assignment
    TypeScript 45-Assignment of Q13 with Node.js

---
+++ main13.js
@@ -0,0 +1,11 @@
+"use strict";
+//ASSIGNMENT NO 13:
+/*
+Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car,
+ and make a list that stores several examples.
+ Use your list to print a series of statements about these items,
+ such as “I would like to own a Honda motorcycle.”
+*/
+console.log("ASSIGNMENT NO 13:");
+let transportation = ["Honda Motorcycle", "Audi", "Honda City"];
+transportation.map((items) => console.log(`I would like to own a ${items}`));
